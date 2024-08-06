0fedb67b54c981ef8c920a62c672aa6fb5c70f13
RobertoVile <viletercerario2882@gmail.com>
2024-08-06 13:45:46 -0300

    Delete atividades bd/connect.py

--- atividades bd/connect.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-def conectar_banco():
-    global banco, cursor
-    banco = sqlite3.connect('guilhermo.db')
-    cursor = banco.cursor()
-    print("Banco conectado com sucesso!")
-    return banco, cursor
